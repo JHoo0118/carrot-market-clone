e64e1ca79d46a664f45e75474a8f9f4a0e286e8e
JHoo0118 <kik3078@gmail.com>
2022-07-17 22:46:23 +0900

    Product Model

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,16 @@
 import type { NextRequest, NextFetchEvent } from 'next/server';
 import { NextResponse } from 'next/server';
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  // // if (req.ua?.isBot) {
-  // //   return new Response("Plz don't be a bot. Be human.", { status: 403 });
-  // // }
-  // if (!req.url.includes("/api")) {
-  //   if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
-  //     return NextResponse.redirect("/enter");
-  //   }
+  // if (req.ua?.isBot) {
+  //   return new Response("Plz don't be a bot. Be human.", { status: 403 });
   // }
-  // //  return NextResponse.json({ ok: true });
+  if (!req.url.includes('/api') && !req.url.includes('/favicon') && !req.url.includes('/_next/static')) {
+    if (!req.url.includes('/enter') && !req.cookies.get('carrotsession')) {
+      const url = req.nextUrl.clone();
+      url.pathname = '/enter';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ import Head from 'next/head';
 
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  console.log(user);
   return (
     <Layout title="홈" hasTabBar>
       <Head>
@@ -17,7 +16,7 @@ const Home: NextPage = () => {
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <Item id={i} key={i} title="iPhone 14" price={99} comments={1} hearts={1} />
         ))}
-        <FloatingButton href="/items/upload">
+        <FloatingButton href="/products/upload">
           <svg
             className="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
