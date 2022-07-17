d69b5b05720c9d73e0d44951683bd58958deed4c
JHoo0118 <kik3078@gmail.com>
2022-07-17 23:12:06 +0900

    Upload API

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -5,6 +5,8 @@ import Layout from '@components/layout';
 import TextArea from '@components/textarea';
 import { useForm } from 'react-hook-form';
 import useMutation from '@libs/client/useMutation';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 interface UploadProductForm {
   name: string;
@@ -13,12 +15,19 @@ interface UploadProductForm {
 }
 
 const Upload: NextPage = () => {
+  const router = useRouter();
   const { register, handleSubmit } = useForm<UploadProductForm>();
   const [uploadProduct, { loading, data }] = useMutation('/api/products');
   const onValid = (data: UploadProductForm) => {
     if (loading) return;
     uploadProduct(data);
   };
+
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/products/${data.product.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack title="Upload Product">
       <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
