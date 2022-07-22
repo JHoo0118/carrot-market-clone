7d195378bc5f15a7f9c5aec1cb78a772b11bfbf0
JHoo0118 <kik3078@gmail.com>
2022-07-23 00:30:44 +0900

    All Posts

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -75,8 +75,9 @@ const CommunityPostDetail: NextPage = () => {
   useEffect(() => {
     if (answerData && answerData.ok) {
       reset();
+      mutate();
     }
-  }, [answerData, reset]);
+  }, [answerData, reset, mutate]);
 
   return (
     <Layout canGoBack>
