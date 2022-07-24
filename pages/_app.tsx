92f1c24a84f6507d85fb1e0ab33951d8683dc164
JHoo0118 <kik3078@gmail.com>
2022-07-24 23:01:34 +0900

    Edit Profile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
+import LoginCheck from '@components/login-check';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig value={{ fetcher: (url: string) => fetch(url).then((response) => response.json()) }}>
       <div className="w-full max-w-xl mx-auto">
+        <LoginCheck />
         <Component {...pageProps} />
       </div>
     </SWRConfig>
