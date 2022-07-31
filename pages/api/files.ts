55b94ebed540b7d7d694d5dd58376b078f17207f
JHoo0118 <kik3078@gmail.com>
2022-07-31 15:17:05 +0900

    Serving Images

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -13,6 +13,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       },
     })
   ).json();
+  console.log(response);
   res.json({
     ok: true,
     ...response.result,
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -35,6 +35,7 @@ const EditProfile: NextPage = () => {
     if (user?.name) setValue('name', user.name);
     if (user?.email) setValue('email', user.email);
     if (user?.phone) setValue('phone', user.phone);
+    if (user?.avatar) setAvatarPreview(`https://imagedelivery.net/I3nSnlW5Zhwjerd3h6xeHg/${user?.avatar}/public`);
   }, [user, setValue]);
   const [editProfile, { data, loading }] = useMutation<EditProfileResponse>(`/api/users/me`);
   const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
@@ -45,18 +46,22 @@ const EditProfile: NextPage = () => {
       });
     }
     if (avatar && avatar.length > 0 && user) {
-      const { id, uploadURL } = await (await fetch(`/api/files`)).json();
+      const { uploadURL } = await (await fetch(`/api/files`)).json();
       const form = new FormData();
       form.append('file', avatar[0], user?.id + '');
-      await fetch(uploadURL, {
-        method: 'POST',
-        body: form,
-      });
+      const {
+        result: { id },
+      } = await (
+        await fetch(uploadURL, {
+          method: 'POST',
+          body: form,
+        })
+      ).json();
       editProfile({
         email,
         phone,
         name,
-        // avatarUrl: CF URL
+        avatarId: id,
       });
     } else {
       editProfile({
