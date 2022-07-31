70125db7d3124651267ddf9f5cabd51ac1b24cb2
JHoo0118 <kik3078@gmail.com>
2022-07-31 14:58:33 +0900

    Direct Upload URL

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -37,20 +37,31 @@ const EditProfile: NextPage = () => {
     if (user?.phone) setValue('phone', user.phone);
   }, [user, setValue]);
   const [editProfile, { data, loading }] = useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
-    console.log(avatar);
-    return;
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === '' && phone === '' && name === '') {
       return setError('formErrors', {
         message: 'Email OR Phone number are required. You need to choose one.',
       });
     }
-    editProfile({
-      email,
-      phone,
-      name,
-    });
+    if (avatar && avatar.length > 0) {
+      const cloudflareRequest = await (await fetch(`/api/files`)).json();
+      console.log(cloudflareRequest);
+      // upload file to CF URL
+      return;
+      editProfile({
+        email,
+        phone,
+        name,
+        // avatarUrl: CF URL
+      });
+    } else {
+      editProfile({
+        email,
+        phone,
+        name,
+      });
+    }
   };
   useEffect(() => {
     if (data && !data.ok && data.error) {
