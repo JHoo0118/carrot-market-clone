51ae231ec7b3e8c5c793ab452bf0bb4b292645a5
JHoo0118 <kik3078@gmail.com>
2022-08-01 21:51:58 +0900

    Dynamic Imports

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
-import type { NextRequest, NextFetchEvent } from 'next/server';
+import { NextRequest, NextFetchEvent, userAgent } from 'next/server';
 import { NextResponse } from 'next/server';
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   // if (req.ua?.isBot) {
   //   return new Response("Plz don't be a bot. Be human.", { status: 403 });
   // }
-  if (!req.url.includes('/api') && !req.url.includes('/favicon') && !req.url.includes('/_next/static')) {
-    if (!req.url.includes('/enter') && !req.cookies.get('carrotsession')) {
+  const ua = userAgent(req);
+  if (ua?.isBot) {
+    // return new Response("Plz don't be a bot. Be human.", { status: 403 });
+  }
+  if (
+    !req.nextUrl.pathname.startsWith('/api') &&
+    !req.nextUrl.pathname.startsWith('/favicon') &&
+    !req.nextUrl.pathname.startsWith('/_next/static')
+  ) {
+    if (!req.nextUrl.pathname.startsWith('/enter') && !req.cookies.get('carrotsession')) {
       const url = req.nextUrl.clone();
       url.pathname = '/enter';
       return NextResponse.redirect(url);
